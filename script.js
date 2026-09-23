1956e97f482dfbbc8f35b05f2228515c4fe77e46
asadumar8ar <umarasad7678@gmail.com>
2026-09-23 14:03:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Viona Bangles - Supabase Connection
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
+const SUPABASE_URL = "https://sqjbmdicbkykfqnqezss.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
 
 const supabase = window.supabase.createClient(
